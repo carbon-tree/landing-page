7b5f7f0b62cfba11c52d01a484156a8c20382ca1
André de Faria Carvlaho <adfariacarvalho@gmail.com>
2024-12-10 09:49:59 -0300

    feat: add team

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Team() {
   return (
     <section className="container mx-auto py-16 px-4">
-      <h1 className="text-5xl font-extrabold mb-12 text-gray-800 text-center">
+      <h1 className="text-5xl font-extrabold mb-12 text-white text-center">
         Our Team
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
@@ -12,7 +12,7 @@ export default function Team() {
             key={index}
             className="bg-white rounded-lg shadow-xl overflow-hidden transform transition duration-300 hover:scale-105"
           >
-            <img src={member.image} alt={member.name} width={400} height={400} className="w-full h-64 object-cover"></img>
+            <img src={member.image} alt={member.name} width={600} height={400} className="w-full h-64 object-cover"></img>
             <div className="p-6">
               <h2 className="text-2xl font-bold mb-2 text-gray-800">
                 {member.name}
@@ -41,24 +41,36 @@ const teamMembers = [
     role: "CEO",
     description:
       "Blockchain technology and sustainability expert, Alexandre leads our mission to revolutionize the carbon market.",
-    image:
-      "https://media.licdn.com/dms/image/v2/D4D03AQFdEu6p9s2jhw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1712708067341?e=1735171200&v=beta&t=2kPuCJehw4A_aArlyRGHHm-B2t320b4uovDr5T01WlY",
-    linkedin: "https://www.linkedin.com/in/andre-de-faria",
+    image: "/team/alexandre.jpg",
+    linkedin: "https://www.linkedin.com/in/alexandre-cruz-web3/",
   },
   {
     name: "André",
     role: "CTO",
     description:
       "With extensive experience in software development, André leads our technological innovations.",
-    image: "https://avatars.githubusercontent.com/u/39014633",
+    image: "/team/andre.jpg",
     linkedin: "https://www.linkedin.com/in//andre-de-faria",
   },
   {
     name: "Panayotis Giannopoulos",
     role: "Legal Advisor",
     description: "Entrepreneur, technology enthusiast since 1983",
-    image:
-      "https://media.licdn.com/dms/image/v2/D4D03AQFS3XeIcdYLRQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1723007679779?e=1735171200&v=beta&t=izKif7BpCrxVzqwW_vO3iRrXQ0tnimoyMiWPmFwIT7Y",
+    image: "/team/panayotis.jpg",
     linkedin: "https://www.linkedin.com/in/panayotis-giannopoulos-br/",
+  },
+  {
+    name: "Leonardo Ferah",
+    role: "CDO",
+    description: "Entrepreneur and professional with 10 years of experience between tradicional and digital marketing.",
+    image: "/team/leonardo.jpg",
+    linkedin: "https://www.linkedin.com/in/leonardoferah/",
+  },
+  {
+    name: "Bruno Candiota",
+    role: "Legal Advisor",
+    description: "With solid experience in administrative routines and customer service.",
+    image: "/team/bruno.jpg",
+    linkedin: "https://www.linkedin.com/in/bruno-candiota-b91a4bb8/",
   },
 ];
